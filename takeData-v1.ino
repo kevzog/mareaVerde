0962a84612be79226b40d4264159ad94f0293b21
Kevzog <kevh.bis@gmail.com>
2018-09-25 13:46:35 -0300

    stable version. sketch for taking data.

---
+++ takeData-v1.ino
@@ -0,0 +1,65 @@
+//switch
+int pinSwitch = 2;
+int valSwitch;
+
+//tomar primer click
+bool firstClick = false;
+
+//printear el inicio
+bool doPrint = true;
+
+//generacion de un nuevo millis
+unsigned long newMillis = 0;
+int sampleTime = 200;
+
+//para tomar solo un valor de millis con el switch en high
+bool takeit = true;
+
+void setup() {
+  pinMode(pinSwitch, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  readSwitch();
+  takeClick();
+  printStart();
+  startTime();
+  takeMillis();
+}
+
+void readSwitch() {
+  valSwitch = digitalRead(pinSwitch);
+}
+
+void takeClick() {
+  if (valSwitch == 1) {
+    firstClick = true;
+  }
+}
+
+void printStart() {
+  if (firstClick && doPrint) {
+    Serial.println("EMPIEZA");
+    doPrint = false;
+  }
+}
+
+void startTime() {
+  if (firstClick) {
+    newMillis++;
+    delay(1);
+  }
+}
+
+void takeMillis() {
+  if (valSwitch && newMillis > sampleTime) {
+    if (takeit) {
+      Serial.print(newMillis);
+      Serial.print(", ");
+      takeit = false;
+    }
+  } else {
+    takeit = true;
+  }
+}
